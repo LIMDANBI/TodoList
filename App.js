3638a901999634a02406aa1ecd6068679760a443
danbibibi <dlaeksql08222@gmail.com>
2022-03-03 16:44:49 +0900

    todolist: title, input components

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+
+import Title from './src/components/Title';
+import Input from './src/components/Input';
 
 export default function App() {
+  const [newTask, setNewTask] = useState('');
+
+  const addTask = () => {
+    alert(`add: ${newTask}`);
+    setNewTask('');
+  };
+
+  const handleTextChange = (text) => {
+    setNewTask(text);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Hello, I'm danbibibi</Text>
       <StatusBar style="auto" />
+      <Title title="todo list"></Title>
+      <Input
+        value = {newTask}
+        onChangeText = {handleTextChange}
+        onSubmitEditing = {addTask}
+      />
+      <Button
+        title = "enter"
+        onPress={()=> {
+          addTask();
+        }}
+      />
     </View>
   );
 }
